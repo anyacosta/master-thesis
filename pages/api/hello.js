3b224172357a7952aa98c56120e7054909bd85d5
Anays Guilarte Acosta <anyacosta94@gmail.com>
2022-08-31 02:09:16 -0400

    fix build

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,7 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-import modelUrl from "../../models/model.json";
-
 export default function handler(req, res) {
-  res.status(200).json(modelUrl);
+  res.status(200).json({ data: "hello" });
 }
